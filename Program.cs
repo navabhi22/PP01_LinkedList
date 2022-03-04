b1432550ad80866d79b29a4ac8ccac5fd5204821
navabhi22 <navabhi22@gmail.com>
2022-03-05 00:22:12 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,11 @@ namespace PP01
     {
         static void Main(string[] args)
         {
-            Node node = new Node(56);
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
             list.Add(70);
             list.Display();
+            list.InsertaAtaParticularPosition(1, 30);
 
         }
     }
