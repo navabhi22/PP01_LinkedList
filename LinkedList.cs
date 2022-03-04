1015bb8b5d582438c5a477fcfcc5898fea526da7
navabhi22 <navabhi22@gmail.com>
2022-03-05 00:32:25 +0530

    Abhinav Added LinkedList

--- LinkedList.cs
+++ LinkedList.cs
@@ -43,35 +43,16 @@ namespace PP01
                 temp = temp.next;
             }
         }
-        public Node InsertaAtaParticularPosition(int position, int data)
+        public Node RemoveFirstNode()
         {
-            if (position < 1)
+            if (this.head == null)
             {
-                Console.WriteLine("Invalid Position");
+                return null;
             }
-            if (position == 1)
-            {
-                var newNode = new Node(data);
-                newNode.next = this.head;
-                head= newNode;
-            }
-            else
-            {
-                while (position-- != 0)
-                {
-                    if (position == 1)
-                    {
-                        Node node = new Node(data);
-                        node.next = this.head.next;
-                        head.next = node;
-                        break;
-                    }
-                    head = head.next;
-                }
-                if (position != 1)
-                    Console.WriteLine("Position out of range");
-            }
-            return head;
+            this.head = this.head.next;
+            return this.head;
+
         }
+
     }
 }
