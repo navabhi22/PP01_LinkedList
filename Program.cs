99291ec2fe96f8e78638fbb766ba68bc2941fa46
navabhi22 <navabhi22@gmail.com>
2022-03-04 22:45:36 +0530

    Abhinav Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList");
+        }
+    }
+}
