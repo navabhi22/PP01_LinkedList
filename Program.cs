47a17c96724e845c29afbfc4cbeeae46bc12fd2f
navabhi22 <navabhi22@gmail.com>
2022-03-04 23:21:04 +0530

    Abhinav Added SimpleLinkedList

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PP01
 {
@@ -7,6 +11,15 @@ namespace PP01
         static void Main(string[] args)
         {
             Console.WriteLine("LinkedList");
+            LinkedList<int> list =new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+
+            foreach (var ele in list)
+            {
+                Console.WriteLine(ele);
+            }
         }
     }
 }
