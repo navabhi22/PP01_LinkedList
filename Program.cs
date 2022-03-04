5b430bf6b44ea24854be715157ef0a454a5b1421
navabhi22 <navabhi22@gmail.com>
2022-03-05 00:29:45 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -12,9 +12,11 @@ namespace PP01
         {
             LinkedList list = new LinkedList();
             list.Add(56);
+            //list.Add(30);
             list.Add(70);
             list.Display();
-            list.InsertaAtaParticularPosition(1, 30);
+            list.InsertaAtaParticularPosition(2, 30);
+            list.Display();
 
         }
     }
